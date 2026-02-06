eca8fe853db4b9179d5baeb697799079719b7235
Jaro Admin <jaro@gmail.com>
2026-02-06 22:05:14 +0400

    initial commit

---
+++ serve.ps1
@@ -0,0 +1,18 @@
+﻿$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:8000/")
+$listener.Start()
+Write-Host "Serving at http://localhost:8000/"
+while ($listener.IsListening) {
+  $context = $listener.GetContext()
+  $path = $context.Request.Url.LocalPath.TrimStart("/")
+  if ([string]::IsNullOrEmpty($path)) { $path = "index.html" }
+  $file = Join-Path (Get-Location) $path
+  if (Test-Path $file) {
+    $bytes = [IO.File]::ReadAllBytes($file)
+    $context.Response.ContentType = "text/html"
+    $context.Response.OutputStream.Write($bytes, 0, $bytes.Length)
+  } else {
+    $context.Response.StatusCode = 404
+  }
+  $context.Response.Close()
+}
